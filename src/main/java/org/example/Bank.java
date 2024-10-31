a208df1770f5b763d1efa4027822a29615310788
sommovir <luca.coraci@gmail.com>
2024-10-31 15:50:51 +0100

    costruttore, e getter and setter

--- src/main/java/org/example/Bank.java
+++ src/main/java/org/example/Bank.java
@@ -2,4 +2,16 @@ package org.example;
 
 public class Bank {
     private String name;
+
+    public Bank(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
