7a1197dc3e3427c7e3fac5ea4f56529792433925
sommovir <luca.coraci@gmail.com>
2024-10-31 15:23:21 +0100

    modificato il main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello 7yyyworld!");
+        System.out.println("Questo l'ha scritto luca");
     }
 }
