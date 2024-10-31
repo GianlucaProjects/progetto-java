ca02207c3cc66eef0bac10e8dbbf1292abeb7633
sommovir <luca.coraci@gmail.com>
2024-10-31 15:24:46 +0100

    altro messaggio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello 7yyyworld!");
         System.out.println("Questo l'ha scritto luca");
+        System.out.println("Altro commento");
     }
 }
