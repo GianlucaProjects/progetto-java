9b4d9553741bd5bfba51bcd9080c83f01240d5f7
sommovir <luca.coraci@gmail.com>
2024-10-31 15:43:41 +0100

    commit squashato
    niente più leak

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,16 @@
 package org.example;
 
 public class Main {
+
+    private final String password = "----------------------";
+
     public static void main(String[] args) {
         System.out.println("Hello 7yyyworld!");
         System.out.println("Questo l'ha scritto luca");
         System.out.println("Altro commento");
         System.out.println("commento di gianluca");
         System.out.println("Terzo commento di Luca!");
+        System.out.println("COMMENTO al momento del leak della password");
+
     }
 }
